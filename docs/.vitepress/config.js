7a7617537d15c681a2c44cc98e07896f1dde0c06
shitong li <1284833243@qq.com>
2024-08-30 09:14:49 +0800

    更改git文件

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,26 +1,27 @@
 import nav from "./nav.mjs"
-import sidebar from "./sidebar.mjs" 
+import sidebar from "./sidebar.mjs"
+
 export default {
-  base: '/vitepress_blog/', 
-  title: 'IT BLOG',
-  description: 'Online Judge 文档',
-  cleanUrls: true,  
-  lastUpdated: true,
-  themeConfig: {
-    editLink: {
-      pattern: 'https://github.com/lstLST123/vitepress_blog/tree/main/docs/:path',
-      text: 'Edit this page on GitHub'
+    base: '/vitepress_blog/',
+    title: 'IT BLOG',
+    description: 'Online Judge 文档',
+    cleanUrls: true,
+    lastUpdated: true,
+    themeConfig: {
+        editLink: {
+            pattern: 'https://github.com/lstLST123/vitepress_blog/tree/main/docs/:path',
+            text: 'Edit this page on GitHub'
+        },
+        siteTitle: "IT BLOG",
+        logo: "/favicon.png",
+        socialLinks: [
+            {icon: "github", link: "https://github.com/vuejs/vitepress"}
+        ],
+        head: [['link', {rel: 'icon', href: '/vitepress_blog/favicon.ico'}]],
+        nav: nav,
+        sidebar: sidebar,
+        search: {
+            provider: 'local'
+        }
     },
-    siteTitle: "IT BLOG",
-    logo: "/favicon.png",
-    socialLinks: [
-      { icon: "github", link: "https://github.com/vuejs/vitepress" }
-    ],
-    head: [['link', { rel: 'icon', href: '/vitepress_blog/favicon.ico' }]],
-    nav: nav,
-    sidebar: sidebar,
-    search: {
-      provider: 'local'
-    }
-  },
 }
